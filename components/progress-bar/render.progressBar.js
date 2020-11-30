347fdb44118d610962ba8f9dc7a166ba933a43c0
DanielGurevich1 <73877308+DanielGurevich1@users.noreply.github.com>
2020-11-30 19:07:25 +0200

    19.18 - time to go home

---
+++ components/progress-bar/render.progressBar.js
@@ -0,0 +1,15 @@
+/**
+ * progress bar element generation
+ * @param {string} titlr progress bar name
+ * @param {number} value Progress bar 
+ * 
+ * */
+
+function renderProgressBar() {
+    console.log('rendering...');
+    console.log(title, value);
+}
+
+
+
+
--- js/main.js/main.js
+++ js/main.js/main.js
@@ -1,9 +1,17 @@
 import { renderSocials }  from "./components/renderSocial/renderSocials.js";
 import { renderSocials }  from "./components/renderSocial/socialsData.js";
+
 import { renderClock } from "./components/clock/renderClock.js";
 
+import { renderProgressBar } from './components/renderProgressBar.js';
 
 
 renderSocials('footer > .row', socialData);
 
 renderClock('.clock');
+
+
+renderProgressBar('web dev', 90);
+renderProgressBar('web dez', 85);
+renderProgressBar('web ux', 40);
+
